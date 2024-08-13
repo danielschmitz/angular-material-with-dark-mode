f2aeeec8a8c6729cb0385602b3ef5e95c0c4cb7c
Daniel Schmitz <danieljfa@gmail.com>
2024-08-13 11:30:11 -0300

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,52 @@
-import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { AsyncPipe } from '@angular/common';
+import { Component, HostBinding, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
+import { Observable, map, shareReplay } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    MatButtonModule,
+    AsyncPipe,
+    RouterOutlet,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'angular-material-with-dark-mode';
+  private breakpointObserver = inject(BreakpointObserver);
+
+  @HostBinding('class')
+  currentTheme: 'light-theme' | 'dark-theme' = 'light-theme';
+  isDarkMode: boolean = false;
+
+  onThemeChanged() {
+    this.isDarkMode = !this.isDarkMode;
+    if (this.isDarkMode) {
+      this.currentTheme = 'dark-theme';
+    } else {
+      this.currentTheme = 'light-theme';
+    }
+  }
+
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
+    .pipe(
+      map((result) => result.matches),
+      shareReplay()
+    );
 }
